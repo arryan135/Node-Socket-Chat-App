a223fb20629b087db2d7115ee8702fc6520bab94
arryan <arryan@umich.edu>
2021-07-20 17:07:39 -0400

    test server <-> client connection with websocket

--- src/index.js
+++ src/index.js
@@ -13,9 +13,23 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+
 // connection is going to fire whenever it gets a new connection
-io.on("connection", () => {
+io.on("connection", (socket) => {
     console.log("New Websocket connection");
+
+    //establish connection between server and client
+    socket.emit("countUpdated", count);
+
+    // listening for event from client
+    socket.on("increment", () => {
+        count++;
+        // socket.emit("countUpdated", count); -> this emits to that single client, but we want to emit to all clients
+
+        // emits to every connection/client;
+        io.emit("countUpdated", count);
+    });
 });
 
 server.listen(port, (req, res) => {
