e5c963cdf60fee4ab1a1116c582c727d2c7a570d
arryan <arryan@umich.edu>
2021-07-20 16:49:12 -0400

    setup websockets via socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+// configure socketio to use it on our server
+const io = socketio(server);  // this makes our server support websocket
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, (req, res) => {
+// connection is going to fire whenever it gets a new connection
+io.on("connection", () => {
+    console.log("New Websocket connection");
+});
+
+server.listen(port, (req, res) => {
     console.log(`Server is up on port ${port}`);
 });
