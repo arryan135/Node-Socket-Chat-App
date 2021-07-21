9a25a5df1377dfe69bfe88e5956faef3a95129ce
arryan <arryan@umich.edu>
2021-07-20 22:20:02 -0400

    add client/server communication via html form

--- src/index.js
+++ src/index.js
@@ -5,30 +5,22 @@ const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-// configure socketio to use it on our server
-const io = socketio(server);  // this makes our server support websocket
+// this makes our server support websocket
+const io = socketio(server);  
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 // connection is going to fire whenever it gets a new connection
 io.on("connection", (socket) => {
     console.log("New Websocket connection");
 
-    //establish connection between server and client
-    socket.emit("countUpdated", count);
+    socket.emit("message", "Welcome!");
 
-    // listening for event from client
-    socket.on("increment", () => {
-        count++;
-        // socket.emit("countUpdated", count); -> this emits to that single client, but we want to emit to all clients
-
-        // emits to every connection/client;
-        io.emit("countUpdated", count);
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);
     });
 });
 
