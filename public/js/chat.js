e69fd66bf8ba4d50d90b9c747d58cfc4dcd470b5
arryan <arryan@umich.edu>
2021-07-20 23:35:24 -0400

    refactor, disable button while sending message and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,30 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
     console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    // disable button after it has been sent and before receiving it
+    $messageFormButton.setAttribute("disabled", "disabled");
 
     const message = e.target.elements.message.value;
 
     // the last fucntional arugment is used for acknowleding events
     socket.emit("sendMessage", message, (error) => {
+        // enable button once it has been delivered
+        $messageFormButton.removeAttribute("disabled");
+        // clear the input after the message has been sent
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+
         if (error){
             return console.log(error);
         }
@@ -19,15 +33,18 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
     });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
     if (!navigator.geolocation){
         return alert("Geolocation is not supported by your browser");
     }
+
+    $sendLocationButton.setAttribute("disabled", "disabled");
 
     navigator.geolocation.getCurrentPosition((location) => {
         const {latitude, longitude} = location.coords;
         socket.emit("sendLocation", {latitude, longitude}, (message) => {
             console.log(message);
+            $sendLocationButton.removeAttribute("disabled");
         });
     });
 })
